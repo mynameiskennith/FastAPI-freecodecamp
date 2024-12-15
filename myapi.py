101efa8b13c7fef73c64931a94c7f7c10ccf761e
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-12-15 06:08:56 +0530

    .

---
+++ myapi.py
@@ -0,0 +1,71 @@
+from typing import Optional
+from fastapi import FastAPI,Path
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = {
+    1:{
+        "name":"John",
+        "age" : 17,
+        "year" : 12
+    }
+}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
+
+@app.get("/")
+def index():
+    return {"name":"First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id:int = Path(description="The ID of the student")):
+    return students[student_id]
+
+@app.get("/get-by-name ")
+def get_student(*,name: Optional[str] = None,test:int):
+    for i in students:
+        if students[i]["name"]==name:
+            return students[i]
+    return {"Data":"Not Found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int,student: Student):
+    if student_id in students:
+        return {"error": "Student Exist"}
+    
+    students[student_id] = student
+    return students[student_id]
+
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id:int, student:UpdateStudent):
+    if student_id not in students:
+        return {"Error": "The student does not exist"}
+    
+    if student.name != None:
+        students[student_id].name= student.name
+    if student.age != None:
+        students[student_id].age= student.age
+    if student.year != None:
+        students[student_id].year= student.year
+    
+    return students[student_id]
+
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id:int):
+    if student_id not in students:
+        return {"Error" : "Not in DB"}
+
+    del students[student_id]
+    return {"Message" : "Student deleted"}
